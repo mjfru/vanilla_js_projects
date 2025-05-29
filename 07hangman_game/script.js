7034f9ea1c32828a7392348ca564057491033ce3
Matt Frutchey <mjfru201@gmail.com>
2025-05-29 12:20:34 -0400

    finish all functionality, including resetting a game

--- 07hangman_game/script.js
+++ 07hangman_game/script.js
@@ -1,14 +1,25 @@
 //* DOM Elements
 const wordElement = document.getElementById("word");
 const wrongLetterElement = document.getElementById("wrong-letters");
-const playAgainButton = document.getElementById("play-again");
+const playAgainButton = document.getElementById("play-button");
 const popup = document.getElementById("popup-container");
 const notification = document.getElementById("notification-container");
 const finalMessage = document.getElementById("final-message");
 const figureParts = document.querySelectorAll(".figure-part");
 
 // Defining a word:
-const words = ["application", "javascript", "interface", "development", "pear"];
+const words = [
+	"application",
+	"javascript",
+	"interface",
+  "node",
+	"development",
+	"java",
+	"react",
+	"algorhithm",
+	"object",
+	"array",
+];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
@@ -40,7 +51,28 @@ function displayWord() {
 
 //* Updating the wrong letters element:
 function updateWrongLettersElement() {
-	console.log("Updated wrong");
+	// console.log("Updated wrong");
+	//* Display the wrong letters guessed
+	wrongLetterElement.innerHTML = `
+    ${wrongLetters.length > 0 ? "<p>Wrong</p>" : ""}
+    ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
+  `;
+
+	//* Drawing the figure
+	figureParts.forEach((part, index) => {
+		const errors = wrongLetters.length;
+		if (index < errors) {
+			part.style.display = "block";
+		} else {
+			part.style.display = "none";
+		}
+	});
+
+	//* Checking losing conditions
+	if (wrongLetters.length === figureParts.length) {
+		finalMessage.innerText = "Not this time. Try again!";
+		popup.style.display = "flex";
+	}
 }
 
 //* Showing a notification:
@@ -75,6 +107,18 @@ window.addEventListener("keydown", (event) => {
 			}
 		}
 	}
+});
+
+//* Reset / Play again
+playAgainButton.addEventListener("click", () => {
+	//* Empty the letter arrays
+	correctLetters.splice(0);
+	wrongLetters.splice(0);
+
+	selectedWord = words[Math.floor(Math.random() * words.length)];
+	displayWord();
+	updateWrongLettersElement();
+	popup.style.display = "none";
 });
 
 displayWord();
