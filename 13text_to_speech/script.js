d293c54285ee099c12eac84db50c8f41009846b2
Matt Frutchey <mjfru201@gmail.com>
2025-07-09 11:18:24 -0400

    use JS to loop over data and create an image / text area for each piece

---
+++ 13text_to_speech/script.js
@@ -0,0 +1,77 @@
+// DOM Elements
+const main = document.querySelector("main");
+const voicesSelect = document.getElementById("voices");
+const textarea = document.getElementById("text");
+const readButton = document.getElementById("read");
+const toggle = document.getElementById("toggle");
+const closeButton = document.getElementById("close");
+
+const data = [
+	{
+		image: "./imgs/drink.jpg",
+		text: "I'm thirsty",
+	},
+	{
+		image: "./imgs/food.jpg",
+		text: "I'm hungry",
+	},
+	{
+		image: "./imgs/tired.jpg",
+		text: "I'm tired",
+	},
+	{
+		image: "./imgs/hurt.jpg",
+		text: "I'm hurt",
+	},
+	{
+		image: "./imgs/happy.jpg",
+		text: "I'm happy",
+	},
+	{
+		image: "./imgs/angry.jpg",
+		text: "I'm angry",
+	},
+	{
+		image: "./imgs/sad.jpg",
+		text: "I'm sad",
+	},
+	{
+		image: "./imgs/scared.jpg",
+		text: "I'm scared",
+	},
+	{
+		image: "./imgs/outside.jpg",
+		text: "I want to go outside",
+	},
+	{
+		image: "./imgs/home.jpg",
+		text: "I want to go home",
+	},
+	{
+		image: "./imgs/school.jpg",
+		text: "I want to go to school",
+	},
+	{
+		image: "./imgs/grandma.jpg",
+		text: "I want to go to grandma's",
+	},
+];
+
+data.forEach(createBox);
+
+// Create speech boxes:
+function createBox(item) {
+	// console.log(item);
+	const { image, text } = item;
+	const box = document.createElement("div");
+	box.classList.add("box");
+	box.innerHTML = `
+    <img src="${image}" alt="${text}"
+    <p class="info">${text}</p>
+  `;
+
+  //TODO Speak event listener
+
+  // Add to main div:
+  main.appendChild(box);
+}
